8a851b9cf96cf98c35afd8e4696056e5fddf5e86
mgrinko <glukym@gmail.com>
2019-03-12 09:08:34 +0200

    Updates

---
+++ precache-manifest.b48e01c208b5af609dc944e10a979830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f285d1dcddde9ee13f7",
+    "url": "/fs_on_dec18-react-datatable/static/css/main.01d9b022.chunk.css"
+  },
+  {
+    "revision": "3f285d1dcddde9ee13f7",
+    "url": "/fs_on_dec18-react-datatable/static/js/main.f0a04a32.chunk.js"
+  },
+  {
+    "revision": "50b6ed16a61471ac8316",
+    "url": "/fs_on_dec18-react-datatable/static/js/runtime~main.c0d4fb66.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/fs_on_dec18-react-datatable/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fs_on_dec18-react-datatable/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c6ac8947f5fe0ee0033a59918f3a8f7",
+    "url": "/fs_on_dec18-react-datatable/index.html"
+  }
+];
