4331afdb0bd160f6818126c77f825dede3198f07
mgrinko <glukym@gmail.com>
2019-03-12 10:19:37 +0200

    Datatable configuration was added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,47 @@ import React from 'react';
 import './App.css';
 import Datatable from './components/Datatable';
 
-const App = () => (
-  <div className="App">
-    <h1>Datatable</h1>
-    <Datatable />
-  </div>
-);
+const peopleColumnConfig = {
+  name: {
+    title: 'Имя', // в таблице колонка будет так называться
+    isSortable: true, // Поиск будет проверять эту и последнюю колонки
+    isSearchable: true,
+  },
+  born: {
+    title: 'Год рождения',
+    isSortable: true, // по этой колонке можно сортировать
+  },
+  sex: { // Только для тех ключей которые есть в columnConfig будут колонки в таблице
+    title: 'Пол',
+  }
+};
+
+class App extends React.Component {
+  state = {
+    people: [],
+    config: peopleColumnConfig,
+  };
+
+  async componentDidMount() {
+    const response = await fetch('https://mate-academy.github.io/fe-program/js/tasks/people/people.json');
+    const people = await response.json();
+
+    this.setState({ people });
+  }
+
+  render () {
+    const { people, config } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Datatable</h1>
+        <Datatable
+          items={people}
+          columnConfig={config}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
