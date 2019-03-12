1ccd96d4bd2b57908a75a5321cfc3823d9107270
mgrinko <glukym@gmail.com>
2019-03-12 10:07:06 +0200

    Datatable Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Datatable from './components/Datatable';
 
 const App = () => (
   <div className="App">
     <h1>Datatable</h1>
+    <Datatable />
   </div>
 );
 
---
+++ src/components/Datatable.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Datatable = () => (
+  <div className="Datatable">Datatable</div>
+);
+
+export default Datatable;
