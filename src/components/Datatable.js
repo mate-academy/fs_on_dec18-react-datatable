198286dc4e1cbec3407d4a10c78b77fd2f386593
mgrinko <glukym@gmail.com>
2019-03-15 17:22:10 +0200

    Pagination was implemented

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import debounce from 'lodash/debounce';
+
+const Pagination = ({ page, perPage, totalCount, onPageChange }) => {
+  const pagesCount = Math.ceil(totalCount / perPage);
+  const pages = Array(pagesCount).fill(0).map((_, i) => i + 1);
+
+  return (
+    <div>
+      {pages.map(p => (
+        <button
+          key={p}
+          style={{ backgroundColor: p === page ? 'yellow' : 'white' }}
+          onClick={() => onPageChange(p)}
+        >
+          { p }
+        </button>
+      ))}
+    </div>
+  );
+};
+
 class Datatable extends React.Component {
   state = {
     sortColumn: null,
     sortAsc: true,
+    perPage: 5,
+    page: 1,
+    query: '',
+    visibleQuery: ''
   };
 
   handleHeaderClick = (key) => {
@@ -30,12 +55,71 @@ class Datatable extends React.Component {
     return items.sort(sortFn);
   }
 
+  handlePerPageChange = (event) => {
+    this.setState({
+      perPage: +event.target.value,
+    });
+  };
+
+  handlePageChange = (page) => {
+    this.setState({ page });
+  };
+
+  handleQueryChange = (event) => {
+    this.setState({
+      visibleQuery: event.target.value,
+    });
+
+    this.updateQuery(event.target.value);
+  };
+
+  updateQuery = debounce((query) => {
+    this.setState({
+      query: query,
+      page: 1,
+    });
+  }, 1000);
+
   render() {
-    const visibleItems = this.getSortedItems();
+    const { page, perPage, query, visibleQuery } = this.state;
+
+    const start = (page - 1) * perPage;
+    const end = start + perPage;
+
+    const queryRegexp = new RegExp(query, 'i');
+
+    const sortedItems = this.getSortedItems();
+    const filteredItems = sortedItems
+      .filter(person => queryRegexp.test(person.name));
+
+    const visibleItems = filteredItems
+      .slice(start, end);
+
     const { config } = this.props;
 
     return (
       <div className="Datatable">
+        <input
+          type="text"
+          value={visibleQuery}
+          onChange={this.handleQueryChange}
+        />
+
+        <select onChange={this.handlePerPageChange} value={this.state.perPage} >
+          <option value="3">3</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="100">100</option>
+        </select>
+
+        <Pagination
+          page={page}
+          perPage={perPage}
+          totalCount={filteredItems.length}
+          onPageChange={this.handlePageChange}
+        />
+
         <table>
           <thead>
             <tr>
